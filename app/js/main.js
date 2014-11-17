42e17117f9810680b2a4d7c3ccb98826d1ee0f9d
Armen <armen_zakaryan@edu.aua.am>
2014-11-18 00:24:52 +0400

    drow works but not so

--- app/js/main.js
+++ app/js/main.js
@@ -60,44 +60,49 @@ app.controller('NodesCtrl', ['$scope', 'NodeSvc',
         $scope.neighbourNodes;
         $scope.links;
         $scope.pointsMap = {};
-        $scope.limit = 6;
+        $scope.limit = 20;
 
-
-        $scope.setPoint = function(nodeNumber, point) {
-            $scope.pointsMap[nodeNumber] = point;
+        function calcCenter(el) {
+            var offset = el.offset();
+            return {
+                x: offset.left + el.width() / 2,
+                y: offset.top + el.height() / 2
+            }
+        }
+        $scope.setPoint = function(nodeNumber, offset) {
+            $scope.pointsMap[nodeNumber] = calcCenter(offset);
         }
 
         $scope.draw = function(ctx) {
 
-            ctx.beginPath();
-            console.log($scope.pointsMap);
-
+            console.log('Points ', $scope.pointsMap);
+            console.log('Links ', $scope.links);
             $scope.links.forEach(function(element, index, array) {
                 var from = $scope.pointsMap[element.nodeID];
                 var to = $scope.pointsMap[element.linkedNodeID]
-                console.log('f', from.x);
-                console.log('hhhhhhhhhhh', from.y);
-                from && ctx.moveTo(+from.x, +from.y);
-                to && ctx.lineTo(+to.x, +to.y);
+                console.log('drawing ', from, to);
+                ctx.beginPath();
+
+                from && ctx.moveTo(+from.x - 20, +from.y - 100);
+                to && ctx.lineTo(+to.x - 20, +to.y - 100);
+
                 ctx.lineWidth = 1;
                 // color
                 ctx.strokeStyle = '#444';
                 // draw it
                 ctx.stroke();
-                console.log('**********');
             });
 
 
             /*
-            ctx.moveTo(100, 100);
-            ctx.lineTo(8000, 8000);
-            ctx.lineWidth = 1;
+            ctx.moveTo(1, 1);
+            ctx.lineTo(100, 100);
+            ctx.lineWidth = 4;
             // color
             ctx.strokeStyle = '#444';
             // draw it
             ctx.stroke();
-            */
-
+*/
 
         }
 
@@ -132,6 +137,53 @@ app.controller('NodesCtrl', ['$scope', 'NodeSvc',
 
 
 //Directives
+app.directive('neighbourElement', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+            scope.setPoint(attr.id, $(element));
+            /*
+            $(element).onPositionChanged(function() {
+                calcCenter($(element));
+            });
+            */
+        }
+    }
+});
+
+app.directive('draw', function() {
+    return {
+        restrict: 'E',
+
+        link: function(scope, element, attr) {
+
+            var ctx = $('<canvas/>', {
+                heiGht: window.innerHeight,
+                widtH: window.innerWidth,
+                class: 'gs-canvas'
+            });
+            $(element).append(ctx);
+            var ctx = element.find('canvas')[0].getContext('2d');
+
+            scope.$on('ngRepeatFinished', function(aa) {
+                reset();
+                scope.draw(ctx);
+            });
+
+            function reset() {
+                element[0].width = element[0].width;
+            }
+
+            $(window).resize(function() {
+                console.log('caled');
+                ctx.width = window.innerWidth;
+                ctx.height = window.innerHeight;
+                //draw();
+            });
+        }
+    }
+});
+
 app.directive('onFinishRender', function($timeout) {
     return {
         restrict: 'A',
@@ -148,54 +200,6 @@ app.directive('onFinishRender', function($timeout) {
         }
     }
 });
-
-app.directive('neighbourElement', function($timeout) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attr) {
-            function calcCenter(el) {
-                var offset = el.offset();
-                return {
-                    x: offset.left + el.width() / 2,
-                    y: offset.top + el.height() / 2
-                }
-            }
-
-            scope.setPoint(attr.id, calcCenter($(element)));
-
-            $(element).onPositionChanged(function() {
-                calcCenter($(element));
-            });
-
-        }
-    }
-});
-
-app.directive('draw', function($timeout) {
-    return {
-        restrict: 'E',
-
-        link: function(scope, element, attr) {
-            var ctx = element.find('canvas')[0].getContext('2d');
-
-            scope.$on('ngRepeatFinished', function(aa) {
-                reset();
-                scope.draw(ctx);
-            });
-
-            function reset() {
-                element[0].width = element[0].width;
-            }
-
-            $(window).resize(function() {
-                console.log('caled');
-                //draw();
-            });
-        }
-    }
-});
-
-
 
 
 
